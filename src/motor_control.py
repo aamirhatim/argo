0f6742e2b32a9e5e36d6feadc47833308da918fb
Aamir Husain <aamir300@gmail.com>
2018-02-20 23:05:25 -0600

    Working basic motor control

--- src/motor_control.py
+++ src/motor_control.py
@@ -2,19 +2,15 @@
 
 import sys
 sys.path.insert(0, "/home/aamirhatim/catkin_ws/src/luggo/lib")
-import roboclaw as Roboclaw ## import RoboClaw library for motor controls
+from roboclaw import Roboclaw ## import RoboClaw library for motor controls
 import rospy
 from ar_track_alvar_msgs.msg import * ## import AR tag custom messages (will remove)
 
 class luggo_obj:
     def __init__(self):
         print "Connecting to motors..."
-        try:
-            self.robo = Roboclaw("/dev/ttyACM0", 115200)
-            self.robo.Open()
-        except:
-            print "Failed to connect to motors! Exiting."
-            return
+        self.robo = Roboclaw("/dev/ttyACM0", 115200)
+        self.robo.Open()
         print "Motors detected!"
 
         print "Setting up ROS object..."
@@ -30,24 +26,27 @@ class luggo_obj:
         else:
             z = 0
 
-        dist = z/(10**300)
-        print "Distance: " + dist
+        dist = int(z/(10**300))
+        print dist
         self.move_luggo(dist)
 
-    def move_luggo(distance):
+    def move_luggo(self, distance):
         address = 0x80
         max_speed = 127
         freeze = 0
-
-        if distance > 1300:
+        if distance == 0:
+            print "Freeze"
+            self.robo.ForwardM1(address, freeze)
+            self.robo.ForwardM2(address, freeze)
+        if distance > 45:
             print "Onward march!"
-            self.robo.ForwardM1(address, max_speed/2)
-            self.robo.ForwardM2(address, max_speed/2)
-        elif distance < 1200:
-            print "Reatreat!"
-            self.robo.BackwardM1(address, max_speed/2)
-            self.robo.BackwardM2(address, max_speed/2)
-        elif distance >= 1200 and distance <= 1300:
+            self.robo.ForwardM1(address, max_speed/4)
+            self.robo.ForwardM2(address, max_speed/4)
+        elif distance < 43 and distance > 0:
+            print "Retreat!"
+            self.robo.BackwardM1(address, max_speed/4)
+            self.robo.BackwardM2(address, max_speed/4)
+        elif distance >= 43 and distance <= 45:
             print "Hold laddy!"
             self.robo.ForwardM1(address, freeze)
             self.robo.ForwardM2(address, freeze)
