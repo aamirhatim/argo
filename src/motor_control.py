54c695ddb66eb7912a08a0f790d0106bd2ca9c5a
Aamir Husain <aamir300@gmail.com>
2018-02-20 19:11:40 -0600

    Add motor controls

--- src/motor_control.py
+++ src/motor_control.py
@@ -2,29 +2,59 @@
 
 import sys
 sys.path.insert(0, "/home/aamirhatim/catkin_ws/src/luggo/lib")
-import roboclaw as robo ## import RoboClaw library for motor controls
+import roboclaw as Roboclaw ## import RoboClaw library for motor controls
 import rospy
-from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers ## import AR tag custom messages (will remove)
+from ar_track_alvar_msgs.msg import * ## import AR tag custom messages (will remove)
 
-def tracker_callback(data):
-    temp = AlvarMarker()
-    temp = data[0]
-    z = temp.pose.position.z
-    print z
+class luggo_obj:
+    def __init__(self):
+        print "Connecting to motors..."
+        try:
+            self.robo = Roboclaw("/dev/ttyACM0", 115200)
+            self.robo.Open()
+        except:
+            print "Failed to connect to motors! Exiting."
+            return
+        print "Motors detected!"
 
-def move_luggo():
-    print "moved!"
+        print "Setting up ROS object..."
+        self.sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.get_dist)
+        # self.pub = rospy.Publisher("/luggo/encoder_count", Encoder)
+        print "Init complete, let's roll homie."
 
-def move():
-    rospy.init_node("luggo_controller")
-    # pub = rospy.Publisher("/luggo/encoder_count", Encoder)
-    sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, tracker_callback)
+    def get_dist(self, data):
+        temp = data.markers
+        if len(temp) == 1:
+            tag = temp[0]
+            z = tag.pose.pose.position.z
+        else:
+            z = 0
+
+        dist = z/(10**300)
+        print "Distance: " + dist
+        self.move_luggo(dist)
+
+    def move_luggo(distance):
+        address = 0x80
+        max_speed = 127
+        freeze = 0
+
+        if distance > 1300:
+            print "Onward march!"
+            self.robo.ForwardM1(address, max_speed/2)
+            self.robo.ForwardM2(address, max_speed/2)
+        elif distance < 1200:
+            print "Reatreat!"
+            self.robo.BackwardM1(address, max_speed/2)
+            self.robo.BackwardM2(address, max_speed/2)
+        elif distance >= 1200 and distance <= 1300:
+            print "Hold laddy!"
+            self.robo.ForwardM1(address, freeze)
+            self.robo.ForwardM2(address, freeze)
 
 def main():
-    try:
-        move()
-    except rospy.ROSInterruptException:
-        pass
+    luggo = luggo_obj()
+    rospy.init_node("luggo_controller")
 
     try:
         rospy.spin()
